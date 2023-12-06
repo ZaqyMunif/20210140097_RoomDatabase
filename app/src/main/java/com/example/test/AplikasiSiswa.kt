f259bf30786cac53925fb975a1a3ee7146d9ba93
ZaqyMunif <zaqymunif17@gmail.com>
2023-12-06 18:57:38 +0700

    menambahkan code class AplikasiSiswa

--- app/src/main/java/com/example/test/AplikasiSiswa.kt
+++ app/src/main/java/com/example/test/AplikasiSiswa.kt
@@ -1,4 +1,17 @@
 package com.example.test
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.test.repositori.ContainerApp
+import com.example.test.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application(){
+    /**
+     * App Container instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
