e65dda9e709334bb6e8bbd7064d422f6ae4c42ba
ZaqyMunif <zaqymunif17@gmail.com>
2023-12-06 20:22:23 +0700

    menambahkan object DestinasiEntry

---
+++ app/src/main/java/com/example/test/ui/theme/Halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.test.ui.theme.Halaman
+
+import com.example.praktikum10.R
+import com.example.test.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
