14895049cc2648a325199a8ac5be0147c601fe5e
ZaqyMunif <zaqymunif17@gmail.com>
2023-12-06 19:26:08 +0700

    menentukan String resource id yang berisi judul yang akan ditampilkan di layar halaman

--- app/src/main/java/com/example/test/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/test/navigasi/DestinasiNavigasi.kt
@@ -5,4 +5,9 @@ interface DestinasiNavigasi {
      * Nama unik untuk menentukan jalur untuk composable
      */
     val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman.
+     */
+    val titleRes: Int
 }
