793b3e976b52789212bff3fa0d6366243fafce2a
ZaqyMunif <zaqymunif17@gmail.com>
2023-12-06 20:29:57 +0700

    menambahkan fun EntrySiswaScreen

--- app/src/main/java/com/example/test/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/test/ui/theme/Halaman/HalamanEntry.kt
@@ -1,9 +1,33 @@
 package com.example.test.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum10.R
+import com.example.test.model.EntryViewModel
+import com.example.test.model.PenyediaViewModel
 import com.example.test.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigetBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
 }
